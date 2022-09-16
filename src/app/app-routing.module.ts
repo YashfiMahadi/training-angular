4bcdffa7d9188a60d7d7bf4b508d2df9129e8b28
YashfiMahadi <yashfiarshi@gmail.com>
2022-09-16 11:19:26 +0700

    feat(Administrator): membuat halaman admin

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  {
+    path:'admin',loadChildren:()=>import('./administrator/administrator.module').then(mod=>mod.AdministratorModule)
+  }
 ];
 
 @NgModule({
